65029cbd61be1ae09f195f5e414e649a3af598d7
Rush1314 <rushdave403@gmail.com>
2025-04-02 21:02:20 +0530

    Refactor string calculator

--- string_calculator/string_calculator.rb
+++ string_calculator/string_calculator.rb
@@ -2,17 +2,31 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    if numbers.start_with?("//")
-      delimiter, numbers = numbers[2..].split("\n", 2)
-    else
-      delimiter = ","
-    end
+    delimiter, numbers = extract_delimiter(numbers)
+    num_list = parse_numbers(numbers, delimiter)
 
-    num_list = numbers.split(/[\n#{Regexp.escape(delimiter)}]/).map(&:to_i)
-    
-    negatives = num_list.select { |n| n.negative? }
-    raise "Negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
+    check_negatives(num_list)
 
     num_list.sum
+  end
+
+  private
+
+  def extract_delimiter(numbers)
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      [parts.first[2..], parts.last]
+    else
+      [",", numbers]
+    end
+  end
+
+  def parse_numbers(numbers, delimiter)
+    numbers.split(/[\n#{Regexp.escape(delimiter)}]/).map(&:to_i)
+  end
+
+  def check_negatives(num_list)
+    negatives = num_list.select(&:negative?)
+    raise "Negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
   end
 end
